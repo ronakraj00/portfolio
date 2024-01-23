8fae617bdc9c437baddfccb998c98d65068b5967
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-01-24 01:13:35 +0530

    add about section

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const skillsSection = document.querySelector(".about-skills");
+const skills = [
+    "React",
+    "NodeJs",
+    "ExpressJs",
+    "MongoDB",
+    "Mongoose",
+    "REST API",
+    "Tailwind CSS",
+    "Git",
+    "HTML",
+    "CSS",
+    "JAVASCRIPT",
+    "GITHUB",
+    "LINUX",
+    "PYTHON",
+    "C",
+    "Arduino",
+    "Raspberry Pi",
+    "Java",
+    "webpack",
+    "vite",
+    "Postman",
+    "Redux-Toolkit",
+];
 
+skills.forEach(skill=>{
+    const skillDiv=document.createElement("div");
+    skillDiv.innerText=skill;
+    skillDiv.classList.add("skill");
+    skillsSection.append(skillDiv);
+})
