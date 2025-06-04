47ade854977f06bfcee27ddb8fc6a82fbf3bcd36
ronakraj00 <12213052@nitkkr.ac.in>
2025-06-04 20:58:02 +0530

    add manifest

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const cacheName = "ronak-resume-v1";
+const filesToCache = [
+  "/",
+  "/index.html",
+  "/resume_ronak_raj.html",
+  "/manifest.json",
+  "/icon.png",
+  "profile_me_picture_2.jpg",
+  "/css-pattern-by-magicpattern_box.png",
+  "/css-pattern-by-magicpattern_cube.png",
+  "/css-pattern-by-magicpattern_diagonal.png",
+  "/css-pattern-by-magicpattern_lines.png",
+  "/css-pattern-by-magicpattern.png",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
